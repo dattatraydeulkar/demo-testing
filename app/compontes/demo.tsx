5aec4357cdc2c68cc59506cc4fc36b8a11040340
Dattatray Deulkar <dattatraydeulkar2003@gmail.com>
2026-04-27 17:51:30 +0530

    demo componet

--- app/compontes/demo.tsx
+++ app/compontes/demo.tsx
@@ -5,6 +5,7 @@ export default function Demo() {
       Hello world!
     </h1>
     <p>This is a simple demo component.</p>
+    <p>It is used to demonstrate how to write tests for React components.</p>
     </>
   );
 }
